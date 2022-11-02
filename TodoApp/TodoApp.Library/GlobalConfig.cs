2659a1de15e6cc39637f434d89a7ebc9b780237b
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-02 18:57:13 +0400

    Global config class added

---
+++ TodoApp/TodoApp.Library/GlobalConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp.Library
+{
+    public static class GlobalConfig
+    {
+        public static string ConnectionString()
+        {
+            return @"Server=DESKTOP-ILO1R6I\SQLEXPRESS;Database=JpWs17Todo;Trusted_Connection=True;";
+        }
+    }
+}
