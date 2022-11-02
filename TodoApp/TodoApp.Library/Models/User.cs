bae38ef9fcf46e58d3ce80909ae5c1fb7cd84adf
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-02 18:55:24 +0400

    Models created

---
+++ TodoApp/TodoApp.Library/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApp.Library.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+}
