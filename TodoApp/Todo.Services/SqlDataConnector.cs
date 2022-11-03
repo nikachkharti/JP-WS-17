354290b480c4c5c0a2ca38cd09a4038fa1bb4205
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-03 21:06:22 +0400

    Sql data connector implemented IDataConnection interface

---
+++ TodoApp/Todo.Services/SqlDataConnector.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.SqlClient;
+using Todo.Services.Interfaces;
+using TodoApp.Library;
+using System.Data;
+
+namespace Todo.Services
+{
+    public class SqlDataConnector : IDataConnection
+    {
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            const string sqlExpression = "sp_AllUsers";
+            List<User> result = new();
+
+            using (SqlConnection connection = new(GlobalConfig.ConnectionString()))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    SqlCommand command = new(sqlExpression, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                    if (reader.HasRows)
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            result.Add(new User
+                            {
+                                UserId = reader.GetInt32(0),
+                                FirstName = reader.GetString(1),
+                                LastName = reader.GetString(2),
+                                FullName = reader.GetString(3),
+                                Email = reader.GetString(4)
+                            });
+                        }
+                    }
+                }
+                catch (SqlException)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+
+                return result;
+            }
+        }
+    }
+}
