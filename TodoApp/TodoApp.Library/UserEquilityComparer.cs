dc5199558d127cb5c76518c51e5d9541dacb6932
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-03 21:33:20 +0400

    Equility comparer overrided for users

---
+++ TodoApp/TodoApp.Library/UserEquilityComparer.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TodoApp.Library
+{
+    public class UserEquilityComparer : IEqualityComparer<User>
+    {
+        public bool Equals(User x, User y) => x.FullName == y.FullName && x.Email == y.Email;
+        public int GetHashCode([DisallowNull] User obj) => obj.FullName.Length;
+    }
+}
