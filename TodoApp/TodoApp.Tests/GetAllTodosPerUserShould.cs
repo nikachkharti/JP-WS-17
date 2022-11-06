ee9f2df2618925eeddda14cb7b96f0da8afcb8dd
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-06 18:16:00 +0400

    GetAllTodosPerUserAsync test added.

---
+++ TodoApp/TodoApp.Tests/GetAllTodosPerUserShould.cs
@@ -0,0 +1,53 @@
+﻿namespace TodoApp.Tests
+{
+    public class GetAllTodosPerUserShould
+    {
+        SqlDataConnector sqlDataConnector = new();
+
+        [Fact]
+        public async Task GetAllTodosForGivenUserAsync()
+        {
+            User userToTest = new User()
+        {
+            UserId = 3,
+            FirstName = "ნიკა",
+            LastName = "ჩხარტიშვილი",
+            FullName = "ნიკა ჩხარტიშვილი",
+            Email = "nika.chkhartishvili@gmail.com"
+        };
+
+            List<Library.Todo> expected = new()
+            {
+                new()
+                {
+                    TodoId = 26,
+                    Title = "ჩავატარო ლექცია",
+                    Description = "ჩავატარო ლექცია winforms -ის შესახებ",
+                    StartDate = DateTime.Parse("2022-11-03 19:22:18.100"),
+                    DueDate = DateTime.Parse("2022-11-03 19:22:18.100"),
+                    Status = "შესასრულებელი",
+                    Priority = "მაღალი",
+                    UserId = 3
+                },
+                new()
+                {
+                    TodoId = 28,
+                    Title = "დავასრულო პროექტი",
+                    Description = "დავასრულო Front-ის პროექტი სამსახურში",
+                    StartDate = DateTime.Parse("2022-11-03 19:23:11.453"),
+                    DueDate = DateTime.Parse("2022-11-03 19:23:11.453"),
+                    Status = "შესასრულებელი",
+                    Priority = "საშუალო",
+                    UserId = 3
+                }
+            };
+
+            List<Library.Todo> actual = await sqlDataConnector.GetAllTodosPerUserAsync(userToTest);
+
+            Assert.Equal(expected, actual, new TodoEquilityComparer());
+        }
+
+
+
+    }
+}
