a39b755562d28d20d7e38e35cb4a10932d7b040f
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-06 18:59:03 +0400

    Login functionality added

--- TodoApp/Todo.Services/Interfaces/IDataConnection.cs
+++ TodoApp/Todo.Services/Interfaces/IDataConnection.cs
@@ -11,10 +11,20 @@ namespace Todo.Services.Interfaces
         public Task<List<User>> GetAllUsersAsync();
 
         /// <summary>
+        /// Login user in system.
+        /// </summary>
+        /// <returns>Logged in user</returns>
+        public Task<User> LoginUserAsync(string email);
+
+        /// <summary>
         /// Get all toos for a logged in user.
         /// </summary>
         /// <param name="model">Logged in user</param>
         /// <returns>List of todos</returns>
         public Task<List<TodoApp.Library.Todo>> GetAllTodosPerUserAsync(User model);
+
+
+        //TODO -- User Login Functioality has to be added
+        //TODO -- User Registration Functioality has to be added
     }
 }
