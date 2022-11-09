d3bc9a86d7e9ceb0f1ef56341b672ad8c856c0d6
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-09 17:46:57 +0400

    RegisterUserAsync test added

---
+++ TodoApp/TodoApp.Tests/RegisterUserAsyncShould.cs
@@ -0,0 +1,26 @@
+﻿namespace TodoApp.Tests
+{
+    public class RegisterUserAsyncShould
+    {
+        [Fact]
+        public async void RegisterNewUserInDatabase()
+        {
+            SqlDataConnector sqlDataConnector = new();
+
+            var all = await sqlDataConnector.GetAllUsersAsync();
+
+            User userToAdd = new()
+            {
+                UserId = all.Last().UserId + 1,
+                FirstName = "ზურაბ",
+                LastName = "ბერიაშვილი",
+                FullName = "ზურაბ ბერიაშვილი",
+                Email = "ZB@gmail.com",
+            };
+
+            Task<User> actual = sqlDataConnector.RegisterUserAsync(userToAdd);
+
+            Assert.Equal(userToAdd, await actual, new UserEquilityComparer());
+        }
+    }
+}
