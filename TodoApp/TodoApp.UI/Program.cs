9b114126f5d96f46d5e259dd9d4f64491596a418
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-13 17:58:13 +0400

    LoginForm made startet file

--- TodoApp/TodoApp.UI/Program.cs
+++ TodoApp/TodoApp.UI/Program.cs
@@ -17,7 +17,7 @@ namespace TodoApp.UI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new RegisterUserForm());
+            Application.Run(new LoginForm());
         }
     }
 }
