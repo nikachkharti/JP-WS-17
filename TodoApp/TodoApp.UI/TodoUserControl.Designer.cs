0f0eafd651a6effdc3b7699303d6435ad32d07b4
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-13 19:01:13 +0400

    Todos user control added

---
+++ TodoApp/TodoApp.UI/TodoUserControl.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace TodoApp.UI
+{
+    partial class TodoUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TodoUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Name = "TodoUserControl";
+            this.Size = new System.Drawing.Size(1256, 756);
+            this.Load += new System.EventHandler(this.TodoUserControl_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ TodoApp/TodoApp.UI/TodoUserControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TodoApp.Library;
+
+namespace TodoApp.UI
+{
+    public partial class TodoUserControl : UserControl
+    {
+        public TodoUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public User LoggedInUser { get; set; }
+
+        private void TodoUserControl_Load(object sender, EventArgs e)
+        {
+            LoggedInUser = LoginForm.LoggedInUser;
+        }
+    }
+}
