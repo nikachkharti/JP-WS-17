3aad6c15ed29263bff2bc37b3831c4813c464f3c
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-27 17:47:09 +0400

    Add method added

--- TodoApp/Todo.Services/Interfaces/IDataConnection.cs
+++ TodoApp/Todo.Services/Interfaces/IDataConnection.cs
@@ -13,33 +13,40 @@ namespace Todo.Services.Interfaces
         /// Get all users.
         /// </summary>
         /// <returns>List of users</returns>
-        public Task<List<User>> GetAllUsers();
+        Task<List<User>> GetAllUsers();
 
         /// <summary>
         /// Login user in system.
         /// </summary>
         /// <returns>Logged in user</returns>
-        public Task<User> LoginUser(string email);
+        Task<User> LoginUser(string email);
 
         /// <summary>
         /// Registers new user.
         /// </summary>
         /// <param name="model">User model to register</param>
         /// <returns>Registered user.</returns>
-        public Task<User> RegisterUser(User model);
+        Task<User> RegisterUser(User model);
 
         /// <summary>
         /// Get all toos for a logged in user.
         /// </summary>
         /// <param name="model">Logged in user</param>
         /// <returns>List of todos</returns>
-        public Task<List<TodoApp.Library.Todo>> GetAllTodosPerUser(User model);
+        Task<List<TodoApp.Library.Todo>> GetAllTodosPerUser(User model);
 
         /// <summary>
         /// Edits todo in database
         /// </summary>
         /// <param name="model">Todo to edit</param>
         /// <returns>Edited todo</returns>
-        public Task<TodoApp.Library.Todo> EditTodo(TodoApp.Library.Todo model);
+        Task<TodoApp.Library.Todo> EditTodo(TodoApp.Library.Todo model);
+
+        /// <summary>
+        /// Adds todo in database
+        /// </summary>
+        /// <param name="model">Todo model</param>
+        /// <returns>Todo model</returns>
+        Task<TodoApp.Library.Todo> AddTodo(TodoApp.Library.Todo model);
     }
 }
