ee9163abbe62b859f625141bf50f7fcd353ace23
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2022-11-30 18:21:00 +0400

    Myprofile user control added

---
+++ TodoApp/TodoApp.UI/MyProfileUserControl.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace TodoApp.UI
+{
+    partial class MyProfileUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.firstNameValue = new System.Windows.Forms.TextBox();
+            this.lastNameValue = new System.Windows.Forms.TextBox();
+            this.emailValue = new System.Windows.Forms.TextBox();
+            this.editBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.firstNameLabel.Location = new System.Drawing.Point(125, 88);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(102, 31);
+            this.firstNameLabel.TabIndex = 0;
+            this.firstNameLabel.Text = "სახელი";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lastNameLabel.Location = new System.Drawing.Point(125, 216);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(81, 31);
+            this.lastNameLabel.TabIndex = 0;
+            this.lastNameLabel.Text = "გვარი";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.emailLabel.Location = new System.Drawing.Point(125, 355);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(137, 31);
+            this.emailLabel.TabIndex = 0;
+            this.emailLabel.Text = "ელ-ფოსტა";
+            // 
+            // firstNameValue
+            // 
+            this.firstNameValue.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.firstNameValue.Location = new System.Drawing.Point(125, 122);
+            this.firstNameValue.Name = "firstNameValue";
+            this.firstNameValue.Size = new System.Drawing.Size(827, 38);
+            this.firstNameValue.TabIndex = 1;
+            // 
+            // lastNameValue
+            // 
+            this.lastNameValue.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lastNameValue.Location = new System.Drawing.Point(125, 250);
+            this.lastNameValue.Name = "lastNameValue";
+            this.lastNameValue.Size = new System.Drawing.Size(827, 38);
+            this.lastNameValue.TabIndex = 2;
+            // 
+            // emailValue
+            // 
+            this.emailValue.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.emailValue.Location = new System.Drawing.Point(125, 398);
+            this.emailValue.Name = "emailValue";
+            this.emailValue.Size = new System.Drawing.Size(827, 38);
+            this.emailValue.TabIndex = 3;
+            // 
+            // editBtn
+            // 
+            this.editBtn.BackColor = System.Drawing.Color.SteelBlue;
+            this.editBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.editBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.editBtn.FlatAppearance.BorderSize = 0;
+            this.editBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.editBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.editBtn.ForeColor = System.Drawing.Color.White;
+            this.editBtn.Location = new System.Drawing.Point(0, 643);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(1189, 47);
+            this.editBtn.TabIndex = 4;
+            this.editBtn.Text = "რედაქტირება";
+            this.editBtn.UseVisualStyleBackColor = false;
+            this.editBtn.Click += new System.EventHandler(this.editBtn_Click);
+            // 
+            // MyProfileUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.editBtn);
+            this.Controls.Add(this.emailValue);
+            this.Controls.Add(this.lastNameValue);
+            this.Controls.Add(this.firstNameValue);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Name = "MyProfileUserControl";
+            this.Size = new System.Drawing.Size(1189, 690);
+            this.Load += new System.EventHandler(this.MyProfileUserControl_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label firstNameLabel;
+        private Label lastNameLabel;
+        private Label emailLabel;
+        private TextBox firstNameValue;
+        private TextBox lastNameValue;
+        private TextBox emailValue;
+        private Button editBtn;
+    }
+}
---
+++ TodoApp/TodoApp.UI/MyProfileUserControl.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Todo.Services;
+using TodoApp.Library;
+
+namespace TodoApp.UI
+{
+    public partial class MyProfileUserControl : UserControl
+    {
+        private User _loggedInUser = new();
+        public MyProfileUserControl(User loggedInUser)
+        {
+            InitializeComponent();
+            _loggedInUser = loggedInUser;
+        }
+
+
+        private async void MyProfileUserControl_Load(object sender, EventArgs e)
+        {
+            var user = await GlobalConfig.ConnectionType.GetUser(_loggedInUser);
+
+            firstNameValue.Text = user.FirstName;
+            lastNameValue.Text = user.LastName;
+            emailValue.Text = user.Email;
+        }
+        private async void editBtn_Click(object sender, EventArgs e)
+        {
+            if (TextBoxesAreValid() && EmailIsValid())
+            {
+                DialogResult editDialog = MessageBox.Show("ნამდვილად გსურთ მონაცემების შეცვლა?", "მონცაემების შეცვლა", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (editDialog == DialogResult.Yes)
+                {
+                    User userToEdit = new()
+                    {
+                        FirstName = firstNameValue.Text,
+                        LastName = lastNameValue.Text,
+                        Email = emailValue.Text,
+                        UserId = _loggedInUser.UserId
+                    };
+
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+                    await GlobalConfig.ConnectionType.EditUser(userToEdit);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+                    DashboardForm dashboardForm = new(userToEdit);
+
+                    MessageBox.Show("ინფორმაცია მომხამრებლის შესახებ წარმატებით შეიცვალა", "ინფორმაციის შეცვლა", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("თქვენს მიერ შემოყვანილი ინფორმაცია არ არის კორექტული", "არასწორი ინფორმაცია", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool TextBoxesAreValid()
+        {
+            return !string.IsNullOrWhiteSpace(firstNameValue.Text) && !string.IsNullOrWhiteSpace(lastNameValue.Text) && !string.IsNullOrWhiteSpace(emailValue.Text);
+        }
+
+        private bool EmailIsValid()
+        {
+            return emailValue.Text.Contains('@') && emailValue.Text.Contains('.');
+        }
+
+
+    }
+}
